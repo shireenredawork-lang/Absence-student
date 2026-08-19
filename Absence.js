7f40109698c6e9176927c01054e5a7216fb7f64a
shireenredawork-lang <shireen.reda.work@gmail.com>
2026-08-19 22:59:02 +0300

    Add files via upload

---
+++ Absence.js
@@ -0,0 +1,48 @@
+function intform(){
+    let nameinput = document.getElementById('studentname');
+    let phoneinput = document.getElementById('studentphone');
+    let otherradio = document.getElementById('otherradio');
+    let otherinput = document.getElementById('otherinput');
+    let error = false;
+    let college = document.getElementsByName('chose')
+    if(nameinput.value==="")
+        {
+            nameinput.style.borderColor = "red";
+            nameinput.focus();
+            error = true;
+        }
+      if(phoneinput.value==="")
+        { 
+            phoneinput.style.borderColor = "red";
+            phoneinput.focus();
+            error = true;
+        }
+            let select = false;
+        for (let type  of college)
+            {
+                if(type.checked)
+                {
+                    select = true;
+                    break;
+                }
+            }
+        if(!select)
+            {
+                alert("برجاء اختيار الكلية");
+                error = true;
+            }
+        else if(otherradio.checked && otherinput.value==="")
+        {
+           otherinput.style.borderColor = "red";
+           otherinput.focus();
+            error = true;
+        }
+        if(error)
+            {
+                return false;
+            }
+        alert("تم الارسال");
+
+        return true;
+
+}
